da4901d41c04b951d53e1b70276da602fadcea87
JoshuaBdeJong <99065626@mydavinci.nl>
2022-09-13 14:41:47 +0200

    zevende commit

---
+++ Feestlunch/Pizza calculator.py
@@ -0,0 +1,24 @@
+print ("Welkom bij 'Pizza Calculator'")
+
+aantal_small = int (input("Hoeveel small pizza's? "))
+aantal_medium = int (input("Hoeveel medium pizza's? "))
+aantal_large = int (input("Hoeveel large pizza's? "))
+
+prijs_small = 4.99
+prijs_medium = 7.99
+prijs_large = 9.99
+
+prijs_totaal_small = prijs_small * aantal_small
+prijs_totaal_medium = prijs_medium * aantal_medium
+prijs_totaal_large = prijs_large * aantal_large
+prijs_totaal = prijs_totaal_small + prijs_totaal_medium + prijs_totaal_large
+
+# Bonnetje
+print (f"""
+--------------------------------------------------------------------
+| {aantal_small} small X 4.99
+| {aantal_medium} medium X 7.99
+| {aantal_large} large X 9.99
+--------------------------------------------------------------------
+| Totaalprijs: {prijs_totaal}
+--------------------------------------------------------------------""")
