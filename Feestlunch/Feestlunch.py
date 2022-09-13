f501a11a15b4fac91f6d29c286f93f8fcd2fc583
JoshuaBdeJong <99065626@mydavinci.nl>
2022-09-13 14:16:59 +0200

    zesde commit

--- Feestlunch/Feestlunch.py
+++ Feestlunch/Feestlunch.py
@@ -1,17 +1,18 @@
-aantal_croissant = 17
+aantal_croissant = int (input('Hoeveel Croissant? '))
 prijs_croissant = 0.39
 
 prijs_croissant_totaal = aantal_croissant * prijs_croissant
 
-aantal_stokbrood = 2
+aantal_stokbrood = int (input('Hoeveel stokbrood? '))
 prijs_stokbrood = 2.78
 
 prijs_stokbrood_totaal = aantal_stokbrood * prijs_stokbrood
 
 kortingsbon = 0.50
-aantal_kortingsbon = 3
+aantal_kortingsbon = int (input('Hoeveel kortingsbonnen? '))
 kortingsbon_totaal = kortingsbon * aantal_kortingsbon
 
-prijs_totaal = prijs_croissant_totaal + prijs_stokbrood_totaal - kortingsbon_totaal
+prijs_totaal = float (prijs_croissant_totaal + prijs_stokbrood_totaal - kortingsbon_totaal)
 
-print (f'De feestlunch kost je bij de bakker' {prijs_totaal} 'euro voor de' {aantal_croissant} 'croissantjes en de' {aantal_stokbrood} 'stokbroden als de' {aantal_kortingsbon} 'kortingsbonnen nog geldig zijn!')
+round (prijs_totaal, 2)
+print ("De feestlunch kost je bij de bakker", ("%.2f" % prijs_totaal), "euro voor de", aantal_croissant, " croissantjes en de", aantal_stokbrood, "stokbroden als de", aantal_kortingsbon, "kortingsbonnen nog geldig zijn!")
--- Feestlunch/Speelhal.py
+++ Feestlunch/Speelhal.py
@@ -7,9 +7,11 @@ entree = aantalpersonen * toegangsticket
 
 seat_per5minuten = 0.37
 aantalminuten = int (input ('Hoeveel minuten wil je zitten? '))
-prijs_seatpp = 3.70
-kosten_seat = prijs_seatpp * aantalpersonen
+aantal_periodes = aantalminuten / 5
+kosten_seat = (aantal_periodes * seat_per5minuten) * aantalpersonen
 
-betalen = entree + kosten_seat
+betalen = float (entree + kosten_seat)
+
+round(betalen, 2)
 
 print (f'Dit geweldige dagje-uit met {aantalpersonen} mensen in de Speelhal met {aantalminuten} minuten VR kost je maar {betalen} euro!')
